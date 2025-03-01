ff0a3ed051f074ea01df1d3d1f39942e9809110c
DivyaaSH23 <divyanshshuklaofficial23@gmail.com>
2025-03-01 14:20:26 +0530

    Name pass as Query Parameter

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +12,10 @@ public class HelloController {
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @GetMapping("/query")  //http://localhost:9000/hello/query?name=Divyansh
+    public String sayHello(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
