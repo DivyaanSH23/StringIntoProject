2b2c54097ef69394719e202114867904657ea2ac
DivyaaSH23 <divyanshshuklaofficial23@gmail.com>
2025-03-01 14:46:27 +0530

    Use POST Request Method and pass first name and last name in the Body

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/hello")  //http://localhost:9000/hello
 public class HelloController {
@@ -23,6 +26,11 @@ public class HelloController {
     @GetMapping("/param/{name}")  //http://localhost:9000/hello/param/Mark
     public String sayHi(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+    @PostMapping("/post") //curl -X POST -H "Content-Type: application/json" -d '{"firstName": "DIVYANSH", "lastName": "SHUKLA"}' "http://localhost:9000/hello/post" -w "\n"
+
+    public String sayHello(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 
 }
